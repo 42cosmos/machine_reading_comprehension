5f1da12f88f698986e92f9688e98bb81c89a7a94
Eunbin Park <parkeb417@gmail.com>
2022-12-26 17:06:55 +0900

    modify scripts

--- run.sh
+++ run.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 mkdir mrc_data
-aws s3 sync s3://{my_bucket}/{my_obj_name} ./mrc_data --no-sign-request
+aws s3 sync s3://docent-information/mrc_data ./mrc_data --no-sign-request
 
-python3 main.py \
+python3 ./main.py \
         --do_train \
         --do_eval \
-        --dset_name klue
 
-python3 main.py \
+sed -i 's/dataset_name: klue/dataset_name: squad_v1_kor/g' config.yaml
+python3 ./main.py \
         --do_train \
         --do_eval \
-        --dset_name squad_v1_kor \
         --load_checkpoint
 
 DOCENT_TRAIN_NUM=$(find ./mrc_data/docent/train -name "*.json" | wc -l)
 DOCENT_VALID_NUM=$(find ./mrc_data/docent/validation -name "*.json" | wc -l)
 
-python main.py \
+echo "Klue and all datasets were trained !"
+echo "And finetune with docent ( training set num: $DOCENT_TRAIN_NUM / valid set num: $DOCENT_VALID_NUM)"
+
+sed -i 's/dataset_name: squad_v1_kor/dataset_name: docent/g' config.yaml
+python3 ./main.py \
         --do_train \
         --do_eval \
-        --load_checkpoint \
-        --dset_name docent
+        --load_checkpoint
